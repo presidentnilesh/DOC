2ff0179ec5daf985eea88623175b7130a0f593e9
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-04-24 22:29:10 +0530

    day6

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 
-let h1 = document.querySelector("h1")
-h1.innerText = "how is my baby !"
+// let h1 = document.querySelector("h1")
+
+// console.dir(h1)
+// // h1.innerText = "how is my baby !"
+// h1.innerHTML ="<i>How is the day </i>" ;
+// h1.hidden = true ;
+
+// let a = document.querySelector("a") ; 
+// a.href = "https://www.google.com" ;      // method 1 
+// a.setAttribute("href" , "https://instagram.com")      // method 2
+
+
+// let image = document.querySelector("img") ;
+// image.setAttribute("src" , "https://cdn.pixabay.com/photo/2015/04/19/08/32/flower-729510_1280.jpg") ;
+
+// let a = document.querySelector("a");
+// console.log(a.getAttribute("href"));
+
+
+let a = document.querySelector("a");
+a.removeAttribute("href");
+
+
