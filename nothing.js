bfa998b0f83b84d8494f63e326a2684d54eefc31
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-05-03 01:15:20 +0530

    Refactor noth.html and nothing.js: clean up styles, enhance form layout, and fix file input functionality

--- nothing.js
+++ nothing.js
@@ -14,11 +14,14 @@
 
     // method to display the uplad file button and make it full functional to upload anything 
 let btn = document.querySelector("#btn");
-let fileinp = document.querySelector("#fileinp")
+let fileinp = document.querySelector("#fileinp");
 btn.addEventListener("click" , function(){
     fileinp.click();
-})
+});
 
 fileinp.addEventListener("change" , function(dets){
-    console.log(dets)
-})
+    const file = dets.target.files[0] ;
+    if (file){
+        btn.textContent = file.name ;
+    }
+});
