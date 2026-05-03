8c160c4e9c6c8d22eafdb90e2c95157e5dbc1a54
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-05-04 01:18:07 +0530

    mouseover and mopusout

---
+++ mouseover.js
@@ -0,0 +1,18 @@
+    // mouse-over and mouse-out
+
+
+let abcd = document.querySelector("#abcd");
+
+abcd.addEventListener("mouseover" , function(){
+    abcd.style.backgroundColor = "yellow" ;
+});
+
+abcd.addEventListener("mouseout" , function(){
+    abcd.style.backgroundColor = "red" ;
+});
+
+    /// mouse-move 
+
+window.addEventListener("mousemove" , function(dets){
+    
+});
