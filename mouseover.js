5b8f17ce00432b157f65d4fad98c2fac6a67b952
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-05-05 21:51:24 +0530

    05/05/2026

--- mouseover.js
+++ mouseover.js
@@ -11,26 +11,26 @@
 //     abcd.style.backgroundColor = "red" ;
 // });
 
-    /// mouse-move 
+    // / mouse-move 
 
-// let abcd = document.querySelector("#abcd")
-// window.addEventListener("mousemove" , function(dets){
-//     abcd.style.top = (dets.clientY-200) + "px" ;
-//     abcd.style.left = (dets.clientX-200) + "px" ;
-// });
+let abcd = document.querySelector("#abcd")
+window.addEventListener("mousemove" , function(dets){
+    abcd.style.top = (dets.clientY-200) + "px" ;
+    abcd.style.left = (dets.clientX-200) + "px" ;
+});
 
         //keyup and keydown 
 
 
 
-document.addEventListener("keydown" , function(e){
-    if(e.key === "Enter"){
-        console.log("enter pressed")
-    }
-});
-document.addEventListener("keyup" , function(e){
-    console.log("released:" , e.key);
-});
+// document.addEventListener("keydown" , function(e){
+//     if(e.key === "Enter"){
+//         console.log("enter pressed")
+//     }
+// });
+// document.addEventListener("keyup" , function(e){
+//     console.log("released:" , e.key);
+// });
 
     // email and password input
 
