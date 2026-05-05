4f6a89d9c372444728d4d670dd822eb4fa41e56b
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-05-05 21:53:34 +0530

    trying to learn

---
+++ eventCapturing.js
@@ -0,0 +1,4 @@
+let a = document.querySelector("a");
+let b = document.querySelector("b");
+let c = document.querySelector("c");
+let btn = document.querySelector("button");
--- preventdefault.js
+++ preventdefault.js
@@ -1,1 +1,18 @@
-// event bublling : jispe event aayega agar ispar listener nahi hua to humaara ebent iske parent par lisgener sundhega air aise 
+// // event bublling : jispe event aayega agar ispar listener nahi hua to humaara ebent iske parent par lisgener sundhega air aise 
+// document.querySelector("#nav") .addEventListener("click" , function(){  // event bubling (if some function is called in the addEventListener and there is no function to  do , then the function id passed to the parent function of the html)
+//     alert("clicked")
+// });
+
+
+        // event bubbling 
+
+
+let ul = document.querySelector("ul");
+ul.addEventListener("click" , function(dets){
+    // console.log(dets.target)     //use to print all the lines that are clicked
+    //dets.target.classList.add("lt")     // add is use to cut the text (we have written the line-through in the html code in lt id )
+    dets.target.classList.toggle("lt")  // toggle -> nahi lagi hoogi to laga deega and lagi hoogi to hata deega 
+
+})
+
+       
